dee8b151c2df898f03472ef1b8f5e99e3d32b9bf
mariamwaleed05 <mariam.waleed2005@gmail.com>
2025-10-23 00:41:15 +0300

    Nav Bar

--- facebookpage/src/index.js
+++ facebookpage/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import CustomNav from './components/CustomNav';
 import './index.css';
+import Nav from './'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <CustomNav/>
+ </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
